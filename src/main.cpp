778dbd81f2246b98a5c173b4e42b8cabc5c83b7d
strawberayi <sreign1103@gmail.com>
2026-07-15 11:15:29 +0800

    Update Q3 Yellow UP duration to 1s

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ const int servoPins[NUM_PULLEYS] = {
 // Default Travel Durations (in milliseconds)
 const unsigned long defaultDurationsUp[NUM_PULLEYS] = {
   7500, 3500, 6000, 2500, // Green Group (Q1-Q4) (Q1 Green UP is 7500)
-  18000, 3500, 1500, 2000, // Yellow Group (Q1-Q4)
+  18000, 3500, 1000, 2000, // Yellow Group (Q1-Q4) (Q3 Yellow UP is 1000)
   300, 300, 300, 800      // Red Group (Q1-Q4) (Q4 Red UP is 800)
 };
 const unsigned long defaultDurationsDown[NUM_PULLEYS] = {
@@ -142,9 +142,9 @@ void loadSettings() {
   
   // Reset memory once to apply new defaults
   int version = preferences.getInt("v", 0);
-  if (version < 24) {
+  if (version < 25) {
     preferences.clear();
-    preferences.putInt("v", 24);
+    preferences.putInt("v", 25);
   }
 
   for (int i = 0; i < NUM_PULLEYS; i++) {
